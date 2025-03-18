9035e8a8c5d7567e7604f42596efac791a00b6f7
NicoOgno <blucode.sur@gmail.com>
2025-03-18 20:56:10 +0100

    router Link working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
-      <Cart />
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </>
     </>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import imgLogo from "../../assets/mbst.png";
+import imgShoppingBag from "../../assets/shoppingBag.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">
+            <img src={imgLogo} alt="MBST Logo" />
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart">
+            <img src={imgShoppingBag} alt="imgShoppingBag" />
+            <span>0</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
