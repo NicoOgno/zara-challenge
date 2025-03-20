51602400eea88ad5ca212fe64842005c1c19caac
NicoOgno <blucode.sur@gmail.com>
2025-03-20 22:18:42 +0100

    Images Standarized

--- src/components/PhonesList/PhonesList.jsx
+++ src/components/PhonesList/PhonesList.jsx
@@ -4,20 +4,22 @@ import styles from "./PhonesList.module.css";
 const PhonesList = ({ phones, handlePhoneDetails }) => {
   return (
     <div className={styles.phonesContainer}>
-      {phones.map((phone) => (
-        <div
-          key={phone.id}
-          className={styles.phoneCard}
-          onClick={() => handlePhoneDetails(phone.id)}
-        >
-          <div className={styles.imageContainer}>
-            <img src={phone.imageUrl} alt={phone.name} />
+      {phones.map((phone) => {
+        return (
+          <div key={phone.id} className={styles.phoneCard}>
+            <div className={styles.imageWrapper}>
+              <img
+                className={styles.phoneImage}
+                src={phone.imageUrl}
+                alt={phone.name}
+              />
+            </div>
+            <p className={styles.phoneBrand}>{phone.brand}</p>
+            <p className={styles.phoneName}>{phone.name}</p>
+            <p className={styles.phonePrice}>{`${phone.basePrice} EUR`}</p>
           </div>
-          <p className={styles.phoneBrand}>{phone.brand}</p>
-          <p className={styles.phoneName}>{phone.name}</p>
-          <p className={styles.phonePrice}>{`${phone.basePrice} EUR`}</p>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
