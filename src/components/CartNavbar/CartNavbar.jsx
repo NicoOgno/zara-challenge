a4931195d964a5ff53c6a8e45ef606c03db508d8
NicoOgno <blucode.sur@gmail.com>
2025-03-20 14:26:54 +0100

    Cart counter implemented

--- src/components/CartNavbar/CartNavbar.jsx
+++ src/components/CartNavbar/CartNavbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "./CartNavbar.module.css";
 import { Link } from "react-router-dom";
 import imgShoppingBag from "../../assets/shoppingBag.png";
+import { useCart } from "../../context/CartContext";
 
 const CartNavbar = () => {
+  const { cart } = useCart();
+
   return (
     <div>
       <Link className="navbarLink" to="/cart">
         <img src={imgShoppingBag} alt="imgShoppingBag" />
-        <span className={styles.cartCount}>0</span>
+        <span className={styles.cartCount}>{cart.length}</span>
       </Link>
     </div>
   );
